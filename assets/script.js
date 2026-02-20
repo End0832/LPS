25417a05f33baecf14333a41d16053d535a1e66e
End0832_ <196808200+End0832@users.noreply.github.com>
2026-02-20 20:09:04 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ async function init() {
 
 
     print("Loading Python...", "add");
-    const scriptResponse = await fetch("../assets/LPS.py");
+    const scriptResponse = await fetch("../assets/LPS.pyw");
     if (!scriptResponse.ok) {
         print("Loading failed.", "replace");
         alert("Python load failed: " + scriptResponse.status);
