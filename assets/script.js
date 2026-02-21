64ce6f9bfad8655e7a74e74067190b0124d5d997
End0832_ <196808200+End0832@users.noreply.github.com>
2026-02-21 16:18:57 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,6 @@ async function init() {
         print("Loading failed.", "replace");
         alert("Pyodide load failed: " + err.message);
     }
-}
 
     print("Loading Python...", "add");
     try {
