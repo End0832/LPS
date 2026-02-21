9f14afb8d520de7b418716e9b3a2a44c2d747bf4
End0832_ <196808200+End0832@users.noreply.github.com>
2026-02-21 10:18:50 +0100

    Update LPS.py

--- assets/LPS.py
+++ assets/LPS.py
@@ -11,66 +11,70 @@ if local:
 from math import inf
 
 class Matrix():
-    def __init__(self, csv):
-        self.csv = []
-        self.titles = []
+    def __init__(self, rcsv):
+        self.csv, self.titles = self.read_csv(rcsv)
+        self.matrix = self.create_matrix(len(self.titles))
+        self.fill()
+
+    def read_csv(self, rcsv):
+        csv = []
+        titles = []
         if local:
             with open(csv, "r", encoding="utf-8") as f:
                 f = f.readlines()
         else:
-            f = csv.splitlines()
-        
+            f = rcsv.splitlines()
         for i in range(1, len(f)):
-            self.csv.append(f[i].strip("\n").split(","))
-        for i in range(0, len(self.csv)):
-            self.titles.append(self.csv[i][0])
-            self.titles.append(self.csv[i][1])
-        self.titles = sorted(set(self.titles))
-        
-        self.matrix = []
-        for _ in range(0, len(self.titles)):
+            csv.append(f[i].strip("\n").split(","))
+        for i in range(0, len(csv)):
+            titles.append(csv[i][0])
+            titles.append(csv[i][1])
+        titles = sorted(set(titles))
+        return csv, titles
+
+    def create_matrix(self, n):
+        matrix = []
+        for _ in range(n):
             nested_list = []
-            for _ in range(0, len(self.titles)):
+            for _ in range(n):
                 nested_list.append(inf)
-            self.matrix.append(nested_list)
+            matrix.append(nested_list)
+        return matrix
 
-        self.fill()
-
-    def get_titles(self):
-        return self.titles
-            
+    def fill(self):
+        for i in range(len(self.matrix)):
+            ways = self.get_ways(self.titles[i])
+            for j in range(len(ways)):
+                a, b, c = ways[j]
+                a = self.get_title_pos(a)
+                b = self.get_title_pos(b)
+                self.matrix[a][b] = int(c)
+                self.matrix[b][a] = int(c)
+            self.matrix[i][i] = 0
+    
     def get(self):
         return self.matrix
 
-    def check(self, val):
-        results = []
-        for i in range(0, len(self.csv)):
-            for j in range(0, len(self.csv[i]) - 1):
+    def get_titles(self):
+        return self.titles
+
+    def get_title_pos(self, title):
+        return self.titles.index(title)
+
+    def get_ways(self, val):
+        ways = []
+        for i in range(len(self.csv)):
+            for j in range(len(self.csv[i]) - 1):
                 if self.csv[i][j] == val:
                     if j == 0:
-                        results.append((self.csv[i][0], self.csv[i][1], self.csv[i][2]))
+                        ways.append((self.csv[i][0], self.csv[i][1], self.csv[i][2]))
                     if j == 1:
-                        results.append((self.csv[i][1], self.csv[i][0], self.csv[i][2]))
-        return results
+                        ways.append((self.csv[i][1], self.csv[i][0], self.csv[i][2]))
+        return ways
 
-    def title_position(self, title):
-        for i in range(0, len(self.titles)):
-            if title == self.titles[i]:
-                return i
+    
 
-    def fill(self):
-        for i in range(0, len(self.titles)):
-            ways = self.check(self.titles[i])
-            for j in range(0, len(ways)):
-                a, b, c = ways[j]
-                a = self.title_position(a)
-                b = self.title_position(b)
-                self.matrix[a][b] = int(c)
-                self.matrix[b][a] = int(c)
-            self.matrix[a][a] = 0
-
-
-
+    
 
 
 class Dijkstra():
@@ -137,9 +141,7 @@ class Dijkstra():
             time = str(seconds) + " s"
         else:
             minutes = 0
-            while seconds >= 60:
-                seconds = seconds - 60
-                minutes = minutes + 1
+            minutes, seconds = devmod(seconds, 60)
             time = str(minutes) + " min " + str(seconds) + " s"
 
         path_str = self.chain_loc[0]
@@ -178,4 +180,5 @@ if local:
     
     window.show()
     app.exec()
+
 
